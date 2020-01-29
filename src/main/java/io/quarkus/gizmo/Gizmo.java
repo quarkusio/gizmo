0b7911390fef81f706af36a94efe7a81eb4095be
Guillaume Smet <guillaume.smet@gmail.com>
2020-01-29 15:28:11 +0100

    Add a constant for the ASM API version we use
    It is used everywhere in Quarkus and it would be better to only update
    it once for Gizmo when we upgrade ASM and have Quarkus align on it.

---
+++ src/main/java/io/quarkus/gizmo/Gizmo.java
@@ -0,0 +1,11 @@
+package io.quarkus.gizmo;
+
+import org.objectweb.asm.Opcodes;
+
+public final class Gizmo {
+
+    public static final int ASM_API_VERSION = Opcodes.ASM7;
+
+    private Gizmo() {
+    }
+}
