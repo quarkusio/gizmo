bb92155f50a55eb9d4af189b40f335ff0b798798
Martin Kouba <mkouba@redhat.com>
2025-03-12 15:16:44 +0100

    Gizmo2: add simple foreach test

---
+++ src/test/java/io/quarkus/gizmo2/LoopTest.java
@@ -0,0 +1,86 @@
+package io.quarkus.gizmo2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.gizmo2.desc.ConstructorDesc;
+import io.quarkus.gizmo2.desc.MethodDesc;
+
+public class LoopTest {
+
+    @Test
+    public void testForEach() {
+        TestClassMaker tcm = new TestClassMaker();
+        Gizmo g = Gizmo.create(tcm);
+        g.class_("io.quarkus.gizmo2.LoopFun", cc -> {
+            cc.implements_(StringListFun.class);
+            cc.constructor(con -> {
+                con.public_();
+                con.body(bc -> {
+                    bc.invokeSpecial(ConstructorDesc.of(Object.class), con.this_());
+                    bc.return_();
+                });
+            });
+            cc.method("apply", mc -> {
+                ParamVar p = mc.parameter("list", List.class);
+                mc.returning(String.class);
+                mc.public_();
+                mc.body(bc -> {
+                    // for(String e : list) {
+                    // return e;
+                    // }
+                    bc.forEach(p, (loop, item) -> {
+                        loop.return_(item);
+                    });
+                });
+            });
+        });
+        assertEquals("foo", tcm.noArgsConstructor(StringListFun.class).apply(List.of("foo", "bar", "baz")));
+    }
+
+    @Test
+    public void testForEachBreak() {
+        TestClassMaker tcm = new TestClassMaker();
+        Gizmo g = Gizmo.create(tcm);
+        g.class_("io.quarkus.gizmo2.LoopFun", cc -> {
+            cc.implements_(StringListFun.class);
+            cc.constructor(con -> {
+                con.public_();
+                con.body(bc -> {
+                    bc.invokeSpecial(ConstructorDesc.of(Object.class), con.this_());
+                    bc.return_();
+                });
+            });
+            cc.method("apply", mc -> {
+                ParamVar p = mc.parameter("t", List.class);
+                mc.returning(String.class);
+                mc.public_();
+                mc.body(bc -> {
+                    // StringBuilder ret = new StringBuilder();
+                    var ret = bc.new_(StringBuilder.class);
+                    bc.forEach(p, (loop, item) -> {
+                        // ret.append(item);
+                        MethodDesc append = MethodDesc.of(StringBuilder.class, "append", StringBuilder.class, String.class);
+                        loop.invokeVirtual(append, ret, item);
+                        // if(item.equals("bar")) break;
+                        loop.if_(loop.eq(item, Constant.of("bar")), isEqual -> {
+                            isEqual.break_(loop);
+                        });
+                    });
+                    bc.return_(bc.withObject(ret).objToString());
+                });
+            });
+        });
+        assertEquals("foobar", tcm.noArgsConstructor(StringListFun.class).apply(List.of("foo", "bar", "baz")));
+    }
+
+    public interface StringListFun {
+
+        String apply(List<String> list);
+
+    }
+
+}
