0df3f0da18ff9ed806675657ccc2cae9cb38fd70
Ladislav Thon <lthon@redhat.com>
2025-04-14 16:54:17 +0200

    make Var an LValueExpr
    Now that `This` is no longer a `Var`, it makes sense for `Var` to simply
    be a subinterface of `LValueExpr`. This improves clarity of the API.

--- src/main/java/io/quarkus/gizmo2/Var.java
+++ src/main/java/io/quarkus/gizmo2/Var.java
@@ -3,7 +3,7 @@ package io.quarkus.gizmo2;
 /**
  * An lvalue expression that is stored in a variable.
  */
-public sealed interface Var extends Expr permits FieldVar, LocalVar, ParamVar {
+public sealed interface Var extends LValueExpr permits FieldVar, LocalVar, ParamVar {
     /**
      * {@return the variable name}
      */
