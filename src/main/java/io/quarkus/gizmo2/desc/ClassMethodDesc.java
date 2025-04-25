470bb2dcc80b75116bde6dc8004e86a50e8ae869
Ladislav Thon <lthon@redhat.com>
2025-04-25 16:09:20 +0200

    add more factory methods to {Class,Interface}MethodDesc
    It is often required to create a method descriptor for a dynamically created
    class, but the method name and type are statically known. The new overloads
    are easier to use in that case.

--- src/main/java/io/quarkus/gizmo2/desc/ClassMethodDesc.java
+++ src/main/java/io/quarkus/gizmo2/desc/ClassMethodDesc.java
@@ -3,6 +3,7 @@ package io.quarkus.gizmo2.desc;
 import java.lang.constant.ClassDesc;
 import java.lang.constant.MethodTypeDesc;
 import java.lang.invoke.MethodType;
+import java.util.List;
 
 import io.quarkus.gizmo2.impl.ClassMethodDescImpl;
 
@@ -13,10 +14,10 @@ public sealed interface ClassMethodDesc extends MethodDesc permits ClassMethodDe
     /**
      * Create a new class method descriptor.
      *
-     * @param owner the method owner (must not be {@code null})
+     * @param owner the class which contains the method (must not be {@code null})
      * @param name the method name (must not be {@code null})
      * @param type the method type (must not be {@code null})
-     * @return the new descriptor (not {@code null})
+     * @return the class method descriptor (not {@code null})
      */
     static ClassMethodDesc of(ClassDesc owner, String name, MethodTypeDesc type) {
         return new ClassMethodDescImpl(owner, name, type);
@@ -25,12 +26,38 @@ public sealed interface ClassMethodDesc extends MethodDesc permits ClassMethodDe
     /**
      * Create a new class method descriptor.
      *
-     * @param owner the method owner (must not be {@code null})
+     * @param owner the class which contains the method (must not be {@code null})
      * @param name the method name (must not be {@code null})
      * @param type the method type (must not be {@code null})
-     * @return the new descriptor (not {@code null})
+     * @return the class method descriptor (not {@code null})
      */
     static ClassMethodDesc of(ClassDesc owner, String name, MethodType type) {
         return of(owner, name, type.describeConstable().orElseThrow(IllegalArgumentException::new));
+    }
+
+    /**
+     * Create a new class method descriptor.
+     *
+     * @param owner the class which contains the method (must not be {@code null})
+     * @param name the name of the method (must not be {@code null})
+     * @param returnType the class of the return type (must not be {@code null})
+     * @param paramTypes the classes of the parameter types (must not be {@code null})
+     * @return the class method descriptor (not {@code null})
+     */
+    static ClassMethodDesc of(ClassDesc owner, String name, Class<?> returnType, Class<?>... paramTypes) {
+        return of(owner, name, MethodType.methodType(returnType, paramTypes));
+    }
+
+    /**
+     * Construct a new class method descriptor.
+     *
+     * @param owner the class which contains the method (must not be {@code null})
+     * @param name the name of the method (must not be {@code null})
+     * @param returnType the class of the return type (must not be {@code null})
+     * @param paramTypes the classes of the parameter types (must not be {@code null})
+     * @return the class method descriptor (not {@code null})
+     */
+    static ClassMethodDesc of(ClassDesc owner, String name, Class<?> returnType, List<Class<?>> paramTypes) {
+        return of(owner, name, MethodType.methodType(returnType, paramTypes));
     }
 }
