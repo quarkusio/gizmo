92a531baf32ba04c1888c43e87e38124cc9c23ff
Ladislav Thon <lthon@redhat.com>
2025-08-06 12:27:54 +0200

    add InstanceExecutableCreator.this_()
    This is a shortcut for accessing `TypeCreator.this_()`, useful especially
    in situations when the original `TypeCreator` is not readily available.

--- src/main/java/io/quarkus/gizmo2/creator/InstanceExecutableCreator.java
+++ src/main/java/io/quarkus/gizmo2/creator/InstanceExecutableCreator.java
@@ -1,8 +1,14 @@
 package io.quarkus.gizmo2.creator;
 
+import io.quarkus.gizmo2.This;
+
 /**
  * A creator for instance executables.
  */
 public sealed interface InstanceExecutableCreator extends ExecutableCreator, InstanceBodyCreator
         permits ConstructorCreator, InstanceMethodCreator {
+    /**
+     * {@return the {@code this} expression}
+     */
+    This this_();
 }
